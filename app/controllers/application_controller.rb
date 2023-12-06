aff4e6b16941808d7ec2c7eb68854a11c285dbb3
AbderrahmanKro <elhairechabderrahman@gmail.com>
2023-12-06 21:27:54 +0100

    Add Users and devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # This method is used to add additional parameters to the devise sign up form
+  # It is used to add the full_name, username and phone_number parameters to the sign up form
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :username, :phone_number])
+  end
 end
